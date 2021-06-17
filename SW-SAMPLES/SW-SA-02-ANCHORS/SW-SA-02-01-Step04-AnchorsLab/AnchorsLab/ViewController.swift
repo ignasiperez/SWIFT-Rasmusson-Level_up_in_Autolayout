54ddbb390a5d2497499db217d7b3e3a780b0df34
Ignasi  Perez <perezignasi@gmail.com>
2021-06-17 12:43:59 +0200

    SW-SA-02-01-Step04-AnchorsLab

---
+++ SW-SAMPLES/SW-SA-02-ANCHORS/SW-SA-02-01-Step04-AnchorsLab/AnchorsLab/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  AnchorsLab
+//
+//  Created by ignasiperez.com on 16/06/2021.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupViews()
+  }
+  
+  
+  func setupViews() {
+    let upperLeftLabel = makeLabel(withText: "upperLeft")
+    let upperRightLabel = makeLabel(withText: "upperRight")
+    let bottomLeftLabel = makeSecondaryLabel(withText: "bottomLeft")
+    let bottomRightButton = makeButton(withText: "bottomRight")
+    
+    view.addSubview(upperLeftLabel)
+    view.addSubview(upperRightLabel)
+    view.addSubview(bottomLeftLabel)
+    view.addSubview(bottomRightButton)
+    
+    upperLeftLabel.topAnchor
+      .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                  constant: 24)
+      .isActive = true
+    upperLeftLabel.leadingAnchor
+      .constraint(equalTo: view.leadingAnchor,
+                  constant: 24)
+      .isActive = true
+    
+    // Challenge
+    upperRightLabel.topAnchor
+      .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                  constant: 24)
+      .isActive = true
+    upperRightLabel.trailingAnchor
+      .constraint(equalTo: view.trailingAnchor,
+                  constant: -24)
+      .isActive = true
+    
+    // Challenge
+    bottomLeftLabel.bottomAnchor
+      .constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                  constant: -24)
+      .isActive = true
+    bottomLeftLabel.leadingAnchor
+      .constraint(equalTo: view.leadingAnchor,
+                  constant: 24)
+      .isActive = true
+    
+    
+    bottomRightButton.bottomAnchor
+      .constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                  constant: -24)
+      .isActive = true
+    bottomRightButton.trailingAnchor
+      .constraint(equalTo: view.trailingAnchor,
+                  constant: -24)
+      .isActive = true
+  }
+  
+  
+  func makeLabel(withText text: String) -> UILabel {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    
+    label.backgroundColor = .systemYellow
+    label.text = text
+    
+    return label
+  }
+  
+  
+  func makeSecondaryLabel(withText text: String) -> UILabel {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.backgroundColor = .systemYellow
+    label.text = text
+    
+    label.font = UIFont.systemFont(ofSize: 12)
+    label.textColor = .systemGray
+    
+    return label
+  }
+  
+  
+  func makeButton(withText text: String) -> UIButton {
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    
+    button.setTitle(text, for: .normal)
+    button.backgroundColor = .systemBlue
+    
+    return button
+  }
+
+}
