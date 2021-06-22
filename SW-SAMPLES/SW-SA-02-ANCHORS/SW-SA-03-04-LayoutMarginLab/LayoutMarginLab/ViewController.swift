f45ca27e33b4a98321975a7c0d916ceef6fb9a5b
Ignasi  Perez <perezignasi@gmail.com>
2021-06-22 10:40:30 +0200

    SW-SA-03-04-LayoutMarginLab

---
+++ SW-SAMPLES/SW-SA-02-ANCHORS/SW-SA-03-04-LayoutMarginLab/LayoutMarginLab/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  LayoutMarginLab
+//
+//  Created by ignasiperez.com on 22/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupViews()
+  }
+  
+  
+  func setupViews() {
+    
+    let redView = UIView()
+    redView.translatesAutoresizingMaskIntoConstraints = false
+    redView.backgroundColor = .systemPink
+    
+    view.addSubview(redView)
+    
+    NSLayoutConstraint.activate([
+      redView.topAnchor
+        .constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+      redView.bottomAnchor
+        .constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+      redView.leadingAnchor
+        .constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+      redView.trailingAnchor
+        .constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+    ])
+  }
+
+
+}
+
